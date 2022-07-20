c37ff73b400fc162e5e7199f300d0c0ee7f0e685
aloycwl <aloycwl@gmail.com>
2022-07-20 17:13:09 +0800

    1 file modified

--- asset/rg.js
+++ asset/rg.js
@@ -1,26 +1,26 @@
-/***
-Initialisation
-Connect needs try catch in case user no metamask
-***/
-CHAIN = 4;
-CA = '0x1747015b28295132d131A524B300b37d9EF2300c';
-CA2 = '0x204DA0e98EAf4c20db8df4aa67F54e0F31888c7B';
-USDT = '0x263b0355B80d384C55dec84DD13DFa10D2141442';
+const CHAIN = 4,
+  CA = '0x1747015b28295132d131A524B300b37d9EF2300c',
+  CA2 = '0x204DA0e98EAf4c20db8df4aa67F54e0F31888c7B',
+  USDT = '0x263b0355B80d384C55dec84DD13DFa10D2141442',
+  u0 = '[]',
+  ua = 'uint256',
+  u1 = { internalType: ua, name: '', type: ua },
+  u2 = { internalType: ua + u0, name: '', type: ua + u0 },
+  ub = 'address',
+  u3 = { internalType: ub, name: '', type: ub },
+  u4 = { internalType: ub + u0, name: '', type: ub + u0 },
+  uc = 'string',
+  u5 = { internalType: uc, name: '', type: uc },
+  u6 = { internalType: uc + u0, name: '', type: uc + u0 };
 try {
   window.ethereum.on('accountsChanged', function (accounts) {
     connect();
   });
 } catch (e) {}
-/***
-Adjust amount
-***/
 function adjust(p) {
   num = Number($('#txtNo').val()) + p;
   $('#txtNo').val(num < 1 || isNaN(num) ? 1 : num);
 }
-/***
-Mint (stake in function)
-***/
 async function mint() {
   oamt = 1e21 * Number($('#txtNo').val());
   amt = oamt.toLocaleString('fullwide', { useGrouping: false });
@@ -28,30 +28,35 @@ async function mint() {
   $('#txtUSDT').html((balUSDT / 1e18).toLocaleString('en-US'));
   if (oamt > balUSDT) {
     $('#mintBtn').html('Minting Mock USDT...'); /*REMOVE THIS IN DEPLOYMENT*/
-    await contract3.methods.MINT(acct).send({ from: acct }); /*REMOVE THIS IN DEPLOYMENT*/
+    await contract3.methods
+      .MINT(acct)
+      .send({ from: acct }); /*REMOVE THIS IN DEPLOYMENT*/
     /*$('#mintBtn').html('Insufficient USDT');
     return;*/
   }
   $('#mintBtn').html('Approving...');
   appr = await contract3.methods.allowance(acct, CA).call();
-  if (appr < oamt) await contract3.methods.approve(CA, amt).send({ from: acct });
+  if (appr < oamt)
+    await contract3.methods.approve(CA, amt).send({ from: acct });
   $('#mintBtn').html('Minting RG...');
-  await contract.methods.mint(_R(), $('#txtNo').val()).send({ from: acct });
+  var _s = location.hash.substring(1).toLowerCase();
+  await contract.methods
+    .mint(
+      _s.length > 1 && _s != acct.toLowerCase()
+        ? _s
+        : '0x0000000000000000000000000000000000000000',
+      $('#txtNo').val()
+    )
+    .send({ from: acct });
   $('#mintBtn').html('Minted');
   disUSDT();
 }
-/***
-Drip function
-***/
 async function drip() {
   $('#claimbtn').html('Claiming...');
   await contract.methods.drip().send({ from: acct });
   disUSDT();
   $('#claimbtn').html('Claimed');
 }
-/***
-Update payment status
-***/
 async function disUSDT() {
   $('#txtRG').html(
     (
@@ -61,22 +66,14 @@ async function disUSDT() {
       ((await contract.methods.balanceOf(acct).call()) + ')')
   );
 }
-/***
-Copy function
-***/
 async function copy() {
   navigator.clipboard.writeText(
     location.href.replace(location.hash, '') + '?#' + acct
   );
   $('#txtRef').html('Copied');
 }
-/***
-Base wallet function
-With ABI
-***/
 async function connect() {
   $('#conBtn').hide();
-  $('#root').show();
   await load(
     [
       {
@@ -117,7 +114,6 @@ async function connect() {
     ],
     CA
   );
-  await load2();
   contract3 = new web3.eth.Contract(
     [
       {
@@ -151,5 +147,6 @@ async function connect() {
     ],
     USDT
   );
+  $('#root').show();
   await disUSDT();
 }
