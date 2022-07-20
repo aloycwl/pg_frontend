a17db5ed55909fb2a50d5764247bae9488548416
aloycwl <aloycwl@gmail.com>
2022-07-20 16:38:21 +0800

    2 files modified

--- asset/web3ac.js
+++ asset/web3ac.js
@@ -47,7 +47,7 @@ async function load(a, b) {
     web3 = new Web3(ethereum);
     acct = await ethereum.request({ method: 'eth_requestAccounts' });
     acct = acct[0];
-    FA = { from: acct };
+    _FA = { from: acct };
     if ((await web3.eth.net.getId()) != CHAIN) {
       await ethereum.request({
         method: 'wallet_switchEthereumChain',
