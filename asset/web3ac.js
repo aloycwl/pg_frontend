72160451d16d47b5677e1f2307dfda96efdfcb54
aloycwl <aloycwl@gmail.com>
2022-07-20 17:13:36 +0800

    1 file modified

--- asset/web3ac.js
+++ asset/web3ac.js
@@ -1,22 +1,3 @@
-const u0 = '[]',
-  ua = 'uint256',
-  u1 = { internalType: ua, name: '', type: ua },
-  u2 = { internalType: ua + u0, name: '', type: ua + u0 },
-  ub = 'address',
-  u3 = { internalType: ub, name: '', type: ub },
-  u4 = { internalType: ub + u0, name: '', type: ub + u0 },
-  uc = 'string',
-  u5 = { internalType: uc, name: '', type: uc },
-  u6 = { internalType: uc + u0, name: '', type: uc + u0 };
-function _R() {
-  var _s = location.hash.substring(1).toLowerCase();
-  return _s.length > 1 && _s != acct.toLowerCase()
-    ? _s
-    : '0x0000000000000000000000000000000000000000';
-}
-async function LB() {
-  return (await contract2.methods.balanceOf(acct).call()) / 1e18;
-}
 async function load(a, b) {
   if (typeof CS != 'undefined')
     $('head').append(
@@ -35,37 +16,5 @@ async function load(a, b) {
       });
     }
     var contract = new web3.eth.Contract(a, b);
-    if (typeof WB != 'undefined') {
-      var web3a = new Web3(WB),
-        contracta = new web3a.eth.Contract(a, b);
-    }
   }
-}
-async function load2() {
-  var contract2 = new web3.eth.Contract(
-    [
-      {
-        inputs: [u3],
-        name: 'balanceOf',
-        outputs: [u1],
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [u3, u1],
-        name: 'approve',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        inputs: [u3, u3],
-        name: 'allowance',
-        outputs: [u1],
-        stateMutability: 'view',
-        type: 'function',
-      },
-    ],
-    CA2
-  );
 }
