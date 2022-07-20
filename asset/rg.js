df53a7c0f6f5318927ae4fcad000fafab7b0b7ee
aloycwl <aloycwl@gmail.com>
2022-07-20 17:31:13 +0800

    2 files modified

--- asset/rg.js
+++ asset/rg.js
@@ -5,57 +5,52 @@ const CHAIN = 4,
   u0 = '[]',
   ua = 'uint256',
   u1 = { internalType: ua, name: '', type: ua },
-  u2 = { internalType: ua + u0, name: '', type: ua + u0 },
+  u2 = { internalType: ua + '[]', name: '', type: ua + '[]' },
   ub = 'address',
   u3 = { internalType: ub, name: '', type: ub },
-  u4 = { internalType: ub + u0, name: '', type: ub + u0 },
+  u4 = { internalType: ub + '[]', name: '', type: ub + '[]' },
   uc = 'string',
   u5 = { internalType: uc, name: '', type: uc },
-  u6 = { internalType: uc + u0, name: '', type: uc + u0 };
+  u6 = { internalType: uc + '[]', name: '', type: uc + '[]' };
 try {
-  window.ethereum.on('accountsChanged', function (accounts) {
+  window.ethereum.on('accountsChanged', function (a) {
     connect();
   });
-} catch (e) {}
-function adjust(p) {
-  num = Number($('#txtNo').val()) + p;
-  $('#txtNo').val(num < 1 || isNaN(num) ? 1 : num);
+} catch (a) {}
+function adjust(a) {
+  (num = Number($('#txtNo').val()) + a),
+    $('#txtNo').val(num < 1 || isNaN(num) ? 1 : num);
 }
 async function mint() {
-  oamt = 1e21 * Number($('#txtNo').val());
-  amt = oamt.toLocaleString('fullwide', { useGrouping: false });
-  balUSDT = await contract3.methods.balanceOf(acct).call();
-  $('#txtUSDT').html((balUSDT / 1e18).toLocaleString('en-US'));
-  if (oamt > balUSDT) {
-    $('#mintBtn').html('Minting Mock USDT...'); /*REMOVE THIS IN DEPLOYMENT*/
-    await contract3.methods
-      .MINT(acct)
-      .send({ from: acct }); /*REMOVE THIS IN DEPLOYMENT*/
-    /*$('#mintBtn').html('Insufficient USDT');
-    return;*/
-  }
-  $('#mintBtn').html('Approving...');
-  appr = await contract3.methods.allowance(acct, CA).call();
-  if (appr < oamt)
-    await contract3.methods.approve(CA, amt).send({ from: acct });
-  $('#mintBtn').html('Minting RG...');
-  var _s = location.hash.substring(1).toLowerCase();
+  (amt = (oamt = 1e21 * Number($('#txtNo').val())).toLocaleString('fullwide', {
+    useGrouping: !1,
+  })),
+    (balUSDT = await contract3.methods.balanceOf(acct).call()),
+    $('#txtUSDT').html((balUSDT / 1e18).toLocaleString('en-US')),
+    oamt > balUSDT &&
+      ($('#mintBtn').html('Minting Mock USDT...'),
+      await contract3.methods.MINT(acct).send({ from: acct })),
+    $('#mintBtn').html('Approving...'),
+    (appr = await contract3.methods.allowance(acct, CA).call()) < oamt &&
+      (await contract3.methods.approve(CA, amt).send({ from: acct })),
+    $('#mintBtn').html('Minting RG...');
+  var a = location.hash.substring(1).toLowerCase();
   await contract.methods
     .mint(
-      _s.length > 1 && _s != acct.toLowerCase()
-        ? _s
+      a.length > 1 && a != acct.toLowerCase()
+        ? a
         : '0x0000000000000000000000000000000000000000',
       $('#txtNo').val()
     )
-    .send({ from: acct });
-  $('#mintBtn').html('Minted');
-  disUSDT();
+    .send({ from: acct }),
+    $('#mintBtn').html('Minted'),
+    disUSDT();
 }
 async function drip() {
-  $('#claimbtn').html('Claiming...');
-  await contract.methods.drip().send({ from: acct });
-  disUSDT();
-  $('#claimbtn').html('Claimed');
+  $('#claimbtn').html('Claiming...'),
+    await contract.methods.drip().send({ from: acct }),
+    disUSDT(),
+    $('#claimbtn').html('Claimed');
 }
 async function disUSDT() {
   $('#txtRG').html(
@@ -69,43 +64,68 @@ async function disUSDT() {
 async function copy() {
   navigator.clipboard.writeText(
     location.href.replace(location.hash, '') + '?#' + acct
-  );
-  $('#txtRef').html('Copied');
+  ),
+    $('#txtRef').html('Copied');
 }
 async function connect() {
-  $('#conBtn').hide();
-  if (typeof CS != 'undefined')
-    $('head').append(
-      $(
-        '<meta name="viewport"content="width=device-width,initial-scale=1.0"><link rel="stylesheet">'
-      ).attr('href', CS)
-    );
-  if (typeof ethereum != 'undefined') {
-    web3 = new Web3(ethereum);
-    acct = await ethereum.request({ method: 'eth_requestAccounts' });
-    acct = acct[0];
-    if ((await web3.eth.net.getId()) != CHAIN) {
-      await ethereum.request({
-        method: 'wallet_switchEthereumChain',
-        params: [{ chainId: '0x' + CHAIN }],
-      });
-    }
-    contract = new web3.eth.Contract(
+  $('#conBtn').hide(),
+    'undefined' != typeof CS &&
+      $('head').append(
+        $(
+          '<meta name="viewport"content="width=device-width,initial-scale=1.0"><link rel="stylesheet">'
+        ).attr('href', CS)
+      ),
+    'undefined' != typeof ethereum &&
+      ((web3 = new Web3(ethereum)),
+      (acct = await ethereum.request({ method: 'eth_requestAccounts' })),
+      (acct = acct[0]),
+      (await web3.eth.net.getId()) != 4 &&
+        (await ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: '0x4' }],
+        })),
+      (contract = new web3.eth.Contract(
+        [
+          {
+            inputs: [],
+            name: 'drip',
+            outputs: [],
+            stateMutability: 'nonpayable',
+            type: 'function',
+          },
+          {
+            inputs: [u3, u1],
+            name: 'mint',
+            outputs: [],
+            stateMutability: 'nonpayable',
+            type: 'function',
+          },
+          {
+            inputs: [u3],
+            name: 'balanceOf',
+            outputs: [u1],
+            stateMutability: 'view',
+            type: 'function',
+          },
+          {
+            inputs: [u3],
+            name: 'downlineCounts',
+            outputs: [u1],
+            stateMutability: 'view',
+            type: 'function',
+          },
+          {
+            inputs: [],
+            name: 'getDrip',
+            outputs: [u1],
+            stateMutability: 'view',
+            type: 'function',
+          },
+        ],
+        CA
+      ))),
+    (contract3 = new web3.eth.Contract(
       [
-        {
-          inputs: [],
-          name: 'drip',
-          outputs: [],
-          stateMutability: 'nonpayable',
-          type: 'function',
-        },
-        {
-          inputs: [u3, u1],
-          name: 'mint',
-          outputs: [],
-          stateMutability: 'nonpayable',
-          type: 'function',
-        },
         {
           inputs: [u3],
           name: 'balanceOf',
@@ -114,56 +134,29 @@ async function connect() {
           type: 'function',
         },
         {
-          inputs: [u3],
-          name: 'downlineCounts',
-          outputs: [u1],
-          stateMutability: 'view',
+          inputs: [u3, u1],
+          name: 'approve',
+          outputs: [],
+          stateMutability: 'nonpayable',
           type: 'function',
         },
         {
-          inputs: [],
-          name: 'getDrip',
+          inputs: [u3],
+          name: 'MINT',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function',
+        },
+        {
+          inputs: [u3, u3],
+          name: 'allowance',
           outputs: [u1],
           stateMutability: 'view',
           type: 'function',
         },
       ],
-      CA
-    );
-  }
-  contract3 = new web3.eth.Contract(
-    [
-      {
-        inputs: [u3],
-        name: 'balanceOf',
-        outputs: [u1],
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [u3, u1],
-        name: 'approve',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        inputs: [u3],
-        name: 'MINT',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        inputs: [u3, u3],
-        name: 'allowance',
-        outputs: [u1],
-        stateMutability: 'view',
-        type: 'function',
-      },
-    ],
-    USDT
-  );
-  $('#root').show();
-  await disUSDT();
+      '0x263b0355B80d384C55dec84DD13DFa10D2141442'
+    )),
+    $('#root').show(),
+    await disUSDT();
 }
