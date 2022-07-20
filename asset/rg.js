1223e64c38dcffbd42f1a898d65a414ed3d4443c
aloycwl <aloycwl@gmail.com>
2022-07-20 13:31:57 +0800

    1 file modified

--- asset/rg.js
+++ asset/rg.js
@@ -4,7 +4,7 @@ Connect needs try catch in case user no metamask
 ***/
 CHAIN = 4;
 CA = '0x913bd35Db4577106f6668d32Cb1B1ab1478ACA91';
-CA2 = '0x73D29551ebC7D7Ac1Bf8E5727b691e0Efa5be290';
+CA2 = '0x204DA0e98EAf4c20db8df4aa67F54e0F31888c7B';
 USDT = '0x263b0355B80d384C55dec84DD13DFa10D2141442';
 try {
   window.ethereum.on('accountsChanged', function (accounts) {
